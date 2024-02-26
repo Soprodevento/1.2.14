21eab4d68bd7b68e9325ce7d7eba37d9f827dbba
Soprodevento <Sashinka2803@gmail.com>
2024-02-27 00:59:04 +0300

    Исправил

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,10 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        double area = 0;
-        double result = calcCircleRadius(area);
-        System.out.println("Радиус окружности " + result );
+        double area =123;
+        calcCircleRaduis(area);
     }
-        public static double calcCircleRadius(double area) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("введите площадь окружности");
-            double S = scanner.nextDouble();
-            area = S/Math.PI;
-            String round = String.format("%.3f", area);
-            System.out.println("Округленный формат"+ round);
-            return area;
+
+    public static void calcCircleRaduis(double area) {
+        System.out.printf("Радиус окружности: %.3f", + Math.sqrt(area / Math.PI));
     }
 }
